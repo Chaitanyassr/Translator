fab1b575bbb2b362c7af977db5a0d9207416b8a3
Chaitanya <chaitanyas0101@gmail.com>
2020-01-25 01:44:33 +0530

    added more input

--- Index.php
+++ Index.php
@@ -7,6 +7,7 @@
 	<label>English</label>
 	<input for="English" name="English" placeholder="Enter the word" required>
 	<label>japanease</label>
+		<input for="japanease" name="japanease" placeholder="" required>
 
 </body>
 </html>

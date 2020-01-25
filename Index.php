5ea9eebf1b3bfc6f081372caf0df5a3ccdc373e7
Chaitanya <chaitanyas0101@gmail.com>
2020-01-25 10:10:22 +0530

    added database

--- Index.php
+++ Index.php
@@ -1,3 +1,19 @@
+
+<?php
+$servername = "localhost";
+$username = "roo";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>

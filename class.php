663a60b1fd494bd64815cfad32a81b091dee60ef
Chaitanya <chaitanyas0101@gmail.com>
2020-01-25 14:12:01 +0530

    added a class

--- class.php
+++ class.php
@@ -1,20 +1,8 @@
 <?php
-echo "test3";
-
-
-
-// echo "test2";
-// $sql = "SELECT id, firstname, lastname FROM MyGuests";
-// $result = mysqli_query($conn, $sql);
-
-// if (mysqli_num_rows($result) > 0) {
-//     // output data of each row
-//     while($row = mysqli_fetch_assoc($result)) {
-//         echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-//     }
-// } else {
-//     echo "0 results";
-// }
-
-// mysqli_close($conn);
+echo "test3"
+class dbex{
+	function extract($typed_word){
+		
+	}
+}
 ?>

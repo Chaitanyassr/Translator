488dc9305168bb204ebc0959460159f7b896f0b5
Chaitanya <chaitanyas0101@gmail.com>
2020-01-25 12:40:56 +0530

    comment code test - 4

--- class.php
+++ class.php
@@ -1,5 +1,5 @@
 <?php
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+$sql = "INSERT INTO `table1` (`srno`, `thankyou`) VALUES ('1', '123');
 VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
@@ -9,18 +9,18 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-echo "test2";
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = mysqli_query($conn, $sql);
+// echo "test2";
+// $sql = "SELECT id, firstname, lastname FROM MyGuests";
+// $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+// if (mysqli_num_rows($result) > 0) {
+//     // output data of each row
+//     while($row = mysqli_fetch_assoc($result)) {
+//         echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//     }
+// } else {
+//     echo "0 results";
+// }
 
-mysqli_close($conn);
+// mysqli_close($conn);
 ?>
